479525fb1b237915b75c02ff6d8be07c582e2915
Andrei <musicmatters@mail.ru>
2022-12-17 16:12:02 +0600

    Задача 2

---
+++ Zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите число b: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+
+if (numberA == numberB)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+    if(numberA > numberB)
+    {
+        Console.WriteLine($"max = {numberA}, min = {numberB}");
+    }
+    else
+        Console.WriteLine($"max = {numberB}, min = {numberA}");
