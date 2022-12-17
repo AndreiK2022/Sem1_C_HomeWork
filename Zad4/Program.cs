cb88a5de16fcf9b44c9ba1441677b2c7540e6f5e
Andrei <musicmatters@mail.ru>
2022-12-17 15:56:10 +0600

    Задача 4

---
+++ Zad4/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число a: ");
+int numberA = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите число b: ");
+int numberB = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine("Введите число c: ");
+int numberC = int.Parse(Console.ReadLine() ?? "");
+
+int max = numberA;
+
+if(numberB > numberA)
+{
+    if(numberB > numberC)
+    {
+    Console.WriteLine($"max = {numberB}");
+    }
+    else
+    Console.WriteLine($"max = {numberC}");
+}
+else
+if(numberA > numberC)
+{
+    Console.WriteLine($"max = {numberA}");
+}
+else
+Console.WriteLine($"max = {numberC}");
