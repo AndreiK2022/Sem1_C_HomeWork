0b5fd1e69c3f9c93e2fdb4b21065e1f6d83efc3d
Andrei <musicmatters@mail.ru>
2022-12-17 18:51:02 +0600

    Задача 8

---
+++ Zad8/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите число a: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+int count = 1;
+
+if(number > 0)
+{
+while(count <= number)
+{
+    if(count%2 == 0)
+    {
+    Console.Write(count + " ");
+    count = count + 1;
+    }
+    else
+    count = count +1;
+}
+}
+else
+if(number <= 0)
+{
+    while(count >= number)
+    {
+        if(count%2 == 0)
+        {
+        Console.Write(count + " ");
+        count = count - 1;
+        }
+        else
+        count = count - 1;
+}
+}
