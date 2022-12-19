a6c286a4e3a491685facbd956582e967a2553a3a
Andrei <musicmatters@mail.ru>
2022-12-19 22:24:16 +0600

    Задача 13

---
+++ Zad13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число:");
+char[] array = Console.ReadLine().ToCharArray();
+if(array.Length < 3)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+Console.WriteLine(array[2]);
