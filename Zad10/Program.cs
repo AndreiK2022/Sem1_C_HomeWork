ef1eb954d37adc9a18b6f088463c25ae6db62b1f
Andrei <musicmatters@mail.ru>
2022-12-19 15:58:27 +0600

    Задача 10

---
+++ Zad10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трёхзначное число:");
+int number = int.Parse(Console.ReadLine());
+int numberSecondDigit = 0;
+
+if(number < 999 && number >= 100)
+{
+    numberSecondDigit = (number % 100) / 10;
+    Console.WriteLine($"Вторая цифра Вашего числа: {numberSecondDigit}");
+}
+else
+
+Console.WriteLine("Введено некорректное число");
