19677874bf37f86c28ed075a6d80ed6b723d6971
Andrei <musicmatters@mail.ru>
2022-12-20 13:18:15 +0600

    Задача 15

---
+++ Zad15/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите цифру дня недели:");
+int number = int.Parse(Console.ReadLine());
+if(number > 0 && number < 8)
+{
+    if(number == 6 || number == 7)
+    {
+        Console.WriteLine("Этот день - выходной");
+    }
+    else
+    Console.WriteLine("Этот день - не выходной, увы");
+}
+else
+Console.WriteLine("Цифра не соответствует заданному диапазону");
