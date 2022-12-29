f9298410e475e38cb7afb2a0bcf8c3ac1a3bad8a
Andrei <musicmatters@mail.ru>
2022-12-29 13:27:27 +0600

    Задача 19

---
+++ Zad19/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+char[] array = Console.ReadLine().ToCharArray();
+if(array.Length != 5)
+{
+    Console.WriteLine("Введено некорректное число");
+}
+else
+if(array[0] == array[4] && array[1] == array[3])
+{
+Console.WriteLine("Введённое число является палиндромом");
+}
+else
+Console.WriteLine("Введённое не является палиндромом");
