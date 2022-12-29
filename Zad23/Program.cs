a40be08df58996caff46bfd64a045f4b6e17eb87
Andrei <musicmatters@mail.ru>
2022-12-29 15:21:54 +0600

    Задача 23

---
+++ Zad23/Program.cs
@@ -0,0 +1,18 @@
+﻿double[] GetPow(int x)
+{
+   double[] array = new double[x];
+   for (int index = 0; index < x; index++)
+   {
+       array[index] = Math.Pow(index+1, 3);
+   }
+   return array;    
+}
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+double[] arrayCube = GetPow(number); 
+for (int i = 0; i < arrayCube.Length; i++)
+{
+   Console.WriteLine($"Куб числа {i+1} равен {arrayCube[i]}");
+}
