e7134b360799b11991e07e56d3f332395083b525
Andrei <musicmatters@mail.ru>
2022-12-29 16:07:37 +0600

    Задача 21

---
+++ Zad21/Program.cs
@@ -0,0 +1,27 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+    return result;
+}
+int x1 = GetNumber("Введите x-координату первой точки");
+int y1 = GetNumber("Введите y-координату первой точки");
+int z1 = GetNumber("Введите z-координату первой точки");
+
+int x2 = GetNumber("Введите x-координату второй точки");
+int y2 = GetNumber("Введите y-координату второй точки");
+int z2 = GetNumber("Введите z-координату второй точки");
+
+double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine(distance);
