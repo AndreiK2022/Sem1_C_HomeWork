e858ca3fdea7752dbfd22a1a8ccc4a822d5b8f2d
Andrei <musicmatters@mail.ru>
2023-01-14 20:59:31 +0600

    Задача 27

---
+++ Zad27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("ввели не число или меньше 1.");
+        }
+    }
+
+    return result;
+}
+int numbers = GetNumber("Введите число");
+string strNumbers = numbers.ToString();
+
+int sum = 0;
+foreach (var item in strNumbers)
+{
+    sum += Convert.ToInt32(item.ToString());
+}
+Console.WriteLine(sum);
